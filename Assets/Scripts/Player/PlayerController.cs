4cf8d0a025d9f4f250a027d58a32fb6301e3f76f
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-05-17 21:39:58 +0900

    Added player Jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,21 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private Rigidbody2D rb;
+    [SerializeField] private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        PlayerMovement();
+        Movement();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
     }
 
-    private void PlayerMovement()
+    private void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 moveDir = new Vector2(horizontalInput, 0);
         transform.Translate(moveDir * speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        Vector2 jump = new Vector2 (0, jumpForce);
+        rb.AddForce(jump, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<GroundTag>())
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<GroundTag>())
+        {
+            isGrounded = false;
+        }
     }
 }
---
+++ Assets/Scripts/Tags/GroundTag.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class GroundTag : MonoBehaviour
+{
+    // Used as a tag to identify game object in scene.
+}
