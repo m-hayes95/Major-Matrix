63d46e0bb6ba74bb864af5a1437505f1792d0106
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-06-17 11:18:13 +0900

    Add comments, and particle system

--- Assets/Scripts/Door/MoveToNextMap.cs
+++ Assets/Scripts/Door/MoveToNextMap.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MoveToNextMap : MonoBehaviour
 {
-    [SerializeField] private int nextScene;
+    [SerializeField, Tooltip("Choose the next scene (by index) that will be loaded when the player enters the door ")] 
+    private int nextScene;
+    [SerializeField,Tooltip("Set the game object transform for the mid visuals of the door - these will spin on update")]
+    private Transform visualsTransform;
     private SpinDoorVisuals spinDoorVisuals;
-    [SerializeField] private Transform visualsTransform;
 
     private void Awake()
     {
@@ -13,11 +15,11 @@ public class MoveToNextMap : MonoBehaviour
     }
     private void Update()
     {
-        spinDoorVisuals.Spin(visualsTransform);
+        spinDoorVisuals.Spin(visualsTransform); // Spin the center of the door for visual effects
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<PlayerController>())
+        if (other.gameObject.GetComponent<PlayerController>()) // When the other game object is a Player, move to the next map
             NextMap();
     }
     private void NextMap()
